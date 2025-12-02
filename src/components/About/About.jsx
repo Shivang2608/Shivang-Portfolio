46efd2f6681297c93c7288b8d33dd5d61e8d61a4
Shivang Sagar <shivang26sagar@gmail.com>
2025-12-02 11:02:25 +0530

    Resume Button Updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1Hxl4zNn8lCdI1b4AURIlMNqhcXMZok3q/view?usp=sharing"
+            href="https://drive.google.com/file/d/1bDI3u3AIQ3YVKpUP0IOQcm49qrfcJI6l/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
